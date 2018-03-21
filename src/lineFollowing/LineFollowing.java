b95689ceaa8d30bef9f3fb5c8f3a2982a7be9672
DaryaLari <daryalari@gmail.com>
2018-03-21 18:52:40 +0300

    more reliable version

--- src/lineFollowing/LineFollowing.java
+++ src/lineFollowing/LineFollowing.java
@@ -22,14 +22,14 @@ public class LineFollowing {
 
 
     public void followLine(){
-        int speed = 200;
-        final double kp = 170, kd = 50, ki = 0.2;
+        int speed = 170;
+        final double kp = 150, kd = 30, ki = 0.1;
         double correction = 0;
         double prevError = 0, error = 0, integral = 0, derivative = 0;
         while(true) {
             leftSample.fetchSample(sampleResults, 0);
             rightSample.fetchSample(sampleResults, 1);
-            System.out.println(sampleResults[0] + " " + sampleResults[1]);
+//            System.out.println(sampleResults[0] + " " + sampleResults[1]);
 
             error = sampleResults[0] - sampleResults[1];
             integral += error;
@@ -48,11 +48,11 @@ public class LineFollowing {
 
 
 
-            System.out.println("LS: " + String.format("%.2f", sampleResults[0]) +
-                    "\nRS: " + String.format("%.2f", sampleResults[1]) +
-                    "\nP: " + String.format("%.2f", error) +
-                    "\nI: " + String.format("%.2f", integral) +
-                    "\nD: " + String.format("%.2f", derivative));
+//            System.out.println("LS: " + String.format("%.2f", sampleResults[0]) +
+//                    "\nRS: " + String.format("%.2f", sampleResults[1]) +
+//                    "\nP: " + String.format("%.2f", error) +
+//                    "\nI: " + String.format("%.2f", integral) +
+//                    "\nD: " + String.format("%.2f", derivative));
 
 //            Delay.msDelay(100);
 
